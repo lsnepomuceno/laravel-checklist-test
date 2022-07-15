1f51ef267090773505ee9d2ae79fff657113583c
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2022-07-15 16:09:57 -0300

    feat: implements render funcion on global handler exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,14 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,15 +43,35 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return response()->json(['message' => 'Registro não encontrado.'], 404);
+        }
+
+        if ($e instanceof ValidationException) {
+            return response()->json(['message' => 'Dados informado(s) inválido(s).', 'errors' => $e->validator->getMessageBag()], 422);
+        }
+
+        if ($e instanceof AuthorizationException) {
+            return response()->json(['message' => 'Autorização não concedida.'], 403);
+        }
+
+        if ($e instanceof AuthenticationException) {
+            return response()->json(['message' => 'Você não está autenticado.'], 401);
+        }
+
+        if ($e instanceof ThrottleRequestsException) {
+            return response()->json(['message' => 'Muitas requisições, tente mais tarde.'], 429);
+        }
+
+        return parent::render($request, $e);
     }
 }
